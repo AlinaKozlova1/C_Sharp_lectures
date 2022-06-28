a833bf67dc439fc1fd258defa77e5d45f8cb216a
Alina <alinakozlova908@gmail.com>
2022-06-28 23:13:21 +0300

    add Example "Hello User"

---
+++ HelloConsole/HelloUser/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.Write("Write your name, pls: ");
+string username = Console.ReadLine();
+Console.Write("Hi, ");
+Console.Write(username);
+// You can write "WriteLine" to start your sentence on a new line
