50a1246f10cf2f97e145e35523d0cf3982f737e5
Alina <alinakozlova908@gmail.com>
2022-06-29 20:09:49 +0300

    add random

--- HelloConsole/Sum/Program.cs
+++ HelloConsole/Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 13;
-int numberB = 25;
-int result = numberA + numberB;
+﻿int a = new Random().Next(1, 11); // the last digit will be 10
+Console.WriteLine(a);
+int b = new Random().Next(10, 21);
+Console.WriteLine(b);
+int result = a+b;
 Console.WriteLine(result);
