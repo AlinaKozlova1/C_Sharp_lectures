72d145e708456410181f66f0814524b2b8cfabe3
Alina <alinakozlova908@gmail.com>
2022-06-29 20:31:57 +0300

    add the fifth example 'Conditions'

---
+++ HelloConsole/Conditions/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter the username: ");
+string username = Console.ReadLine(); // get data from terminal by this command 
+
+if (username.ToLower == "dina") // "tolower" makes capital letters
+{
+    Console.WriteLine("Dina, what the hell are you doing here???");
+}
+else 
+{
+    Console.Write("Hey, ");
+    Console.WriteLine(username);    
+}
