be87ee48e24c0b3b524f178f50213ad4470bc381
Alina <alinakozlova908@gmail.com>
2022-07-03 17:21:16 +0300

    add the first array file

---
+++ Lesson_2/MetodArray/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = { 61, 27, 43, 45, 45, 6, 17, 58 };
+int n = array.Length;
+int find = 45;
+int index = 0;
+
+while (index<n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine("index = " + index);
+        break;
+    }
+
+    index++;
+}
